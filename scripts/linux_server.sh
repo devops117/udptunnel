8791ea93fdb34f78d2e129e46555175975082453
devops117 <55235206+devops117@users.noreply.github.com>
2023-07-31 02:44:21 +0000

    switch to nftables from nftables
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>

--- scripts/linux_server.sh
+++ scripts/linux_server.sh
@@ -2,4 +2,7 @@
 
 ifconfig tun0 10.0.0.1 pointopoint 10.0.0.2 mtu 1400 up
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -s 10.0.0.2/32 -o eth0 -j MASQUERADE
+# iptables -t nat -A POSTROUTING -s 10.0.0.2/32 -o eth0 -j MASQUERADE
+sudo nft add table nat
+sudo nft add chain nat postrouting { type nat hook postrouting priority 100 \; }
+nft add rule ip nat postrouting oifname eth0 ip saddr 10.0.0.2 counter masquerade
